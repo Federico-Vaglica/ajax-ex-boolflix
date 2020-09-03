5f086593987b26c4de89caf1ce0496b083bd060f
FedericoV <vaglicafederico95@gmail.com>
2020-09-03 10:30:16 +0200

    Ottimizzazione con  function (prima mileStone)

--- script.js
+++ script.js
@@ -23,8 +23,7 @@ $(document).ready(function(){
 
     function insertMovie (){
 
-        var source = $('#day-template').html();
-        var template = Handlebars.compile(source);
+       
 
         var dati = $('input').val();
         $('.movies').empty();
@@ -37,22 +36,47 @@ $(document).ready(function(){
                 'language': 'it-IT'
             },
             success: function(a){
-                var movie= a.results;
-                for ( var i = 0; i < movie.length; i++ ){                                        
-                    var thisMovie={
-                        titolo: movie[i].title,
-                        titoloOriginale: movie[i].original_title,
-                        lingua: movie[i].original_language,
-                        voto: movie[i].vote_average
-                    }
-                    console.log(thisMovie)
-                    var html = template(thisMovie);
-                    $('.movies').append(html);
+                if (a.total_results > 0){
+                    printMovie(a.results);
+                }else {
+                    noResults();
                 }
             },
             error: function(){
               console.log('Errore');
             }
           });
+      }
+
+
+      function printMovie (a){
+
+        var source = $('#day-template').html();
+        var template = Handlebars.compile(source);
+        
+        for ( var i = 0; i < a.length; i++ ){                                        
+            var thisMovie={
+                titolo: a[i].title,
+                titoloOriginale: a[i].original_title,
+                lingua: a[i].original_language,
+                voto: a[i].vote_average
+            }
+            console.log(thisMovie)
+            var html = template(thisMovie);
+            $('.movies').append(html);
+        }
+      }
+
+
+      function noResults() {
+        var source = $('#noResults').html();
+        var template = Handlebars.compile(source);
+
+        var context = {
+            noResults: 'Non ci sono risultati'
+        }
+
+        var html = template(context);
+        $('.movies').append(html);
       }
 });
